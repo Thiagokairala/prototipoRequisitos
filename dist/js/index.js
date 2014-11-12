c1f7b4adb069adfed4613f9b74fa3870bcc8bbcc
Eduardo Brasil <thiagor.kairala@gmail.com>
2014-11-12 16:28:38 -0200

    First commit of the prototype

---
+++ dist/js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('#login').click(function() {
+		var name = /thiagokairala/,
+			password = /123456/,
+			formName = $('#user_name').val(),
+			formPassword = $('#password').val();
+
+		if(formName.match(name) && formPassword.match(password)) {
+			$('#login_form').submit();
+		} else {
+			$('#wrong_credentials').fadeIn('slow');
+		}
+
+	});
+});
